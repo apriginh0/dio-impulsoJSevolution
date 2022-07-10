d0b0a3d42820a28dea4b8bef44a22942e64fb3b5
Diego Aprigio <57737033+apriginh0@users.noreply.github.com>
2022-07-10 03:04:56 -0300

    introducao ao Javascript

---
+++ Introdução ao JS/TESTE/teste.js
@@ -0,0 +1,13 @@
+// var y = 0;
+var x = 1;
+/* var b = 2;
+ var z = 3;   
+ var s = 4; */
+ var a = 5
+// var b = 2;
+// var z = 3;   Usando ctrl+/ é possível comitar tudo selecionado
+// var s = 4;
+const PRECO = 2;
+var desconto = 0.2;
+var total = PRECO - PRECO*desconto;
+console.log(total);
