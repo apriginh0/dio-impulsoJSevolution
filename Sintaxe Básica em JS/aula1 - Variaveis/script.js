fd2e5ab8215ef02ceea1618d5f16a100e3c88270
Diego Aprigio <57737033+apriginh0@users.noreply.github.com>
2022-07-10 23:36:24 -0300

    Sintaxe basica em Javascript

---
+++ Sintaxe Básica em JS/aula1 - Variaveis/script.js
@@ -0,0 +1,18 @@
+// Tipos primitivos
+
+// Boolean
+var vDuF = false;
+console.log(vDuF);
+console.log(typeof(vDuF));
+console.log("");
+
+// Number
+var anyNumber = 2;
+console.log(anyNumber);
+console.log(typeof(anyNumber));
+console.log("");
+
+// String
+var myName = 'Diego';
+console.log(myName);
+console.log(typeof(myName));
